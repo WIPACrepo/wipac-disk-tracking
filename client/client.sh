6246546333c77190f46117db68b37c9c89386bd5
Patrick Meade <pmeade@icecube.wisc.edu>
2024-10-21 20:10:10 -0500

    Final clean up for merging

--- client/client.sh
+++ client/client.sh
@@ -2,8 +2,8 @@
 # run the testing client
 
 export BASE_URL=${HEALTH_URL:="http://localhost:8080"}
-export CLIENT_ID=${CLIENT_ID:="long-term-archive"}  # TODO: CHANGE THIS TO disk-tracking !!!
-export CLIENT_SECRET=${CLIENT_SECRET:="$(<keycloak-client-secret)"}
+export CLIENT_ID=${CLIENT_ID:="long-term-archive"}                   # you may need a different ID
+export CLIENT_SECRET=${CLIENT_SECRET:="$(<keycloak-client-secret)"}  # if you use a different secret
 export OAUTH_URL=${OAUTH_URL:="https://keycloak.icecube.wisc.edu/auth/realms/IceCube"}
 export REST_URL=${REST_URL:="http://localhost:8080/api/v1"}
 
